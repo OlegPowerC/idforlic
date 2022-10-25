671e0beb48ee3d8ce8bf4078ce3ed02b6b593f25
oleg@powerc.ru <OlegPowerC>
2022-10-25 13:50:12 +0300

    Add linux ID

--- idforlic.go
+++ idforlic.go
@@ -3,19 +3,31 @@ package idforlic
 import (
 	"errors"
 	"golang.org/x/sys/windows/registry"
+	"io/ioutil"
+	"os"
 	"runtime"
 )
 
+const (
+	linuxMfile    = `/var/lib/dbus/machine-id`
+	linuxRHfile   = `/etc/machine-id`
+	windowsRegKey = `SOFTWARE\Microsoft\Cryptography`
+)
+
 func GetID() (string, error) {
-	if runtime.GOOS == "windows" {
+	switch runtime.GOOS {
+	case "windows":
 		return winGetID()
-	} else {
-		return "", errors.New("Unknown OS")
+		break
+	case "linux":
+		return linuxGetID()
+		break
 	}
+	return "", errors.New("Unknown OS")
 }
 
 func winGetID() (wGuid string, wGuidErr error) {
-	RegKey, RegKeyErr := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Cryptography`, registry.QUERY_VALUE|registry.WOW64_64KEY)
+	RegKey, RegKeyErr := registry.OpenKey(registry.LOCAL_MACHINE, windowsRegKey, registry.QUERY_VALUE|registry.WOW64_64KEY)
 	if RegKeyErr != nil {
 		return "", RegKeyErr
 	}
@@ -25,4 +37,27 @@ func winGetID() (wGuid string, wGuidErr error) {
 		return "", KeyErr
 	}
 	return KeyValue, nil
+}
+
+func linuxGetID() (linuxId string, linuxIdErr error) {
+	filecheck := linuxMfile
+	_, fileExistError := os.Stat(filecheck)
+	if os.IsNotExist(fileExistError) {
+		filecheck = linuxRHfile
+		_, fileExistError = os.Stat(filecheck)
+		if os.IsNotExist(fileExistError) {
+			return "", errors.New("Can not get ID on this OS")
+		}
+	}
+
+	LinuxIDFile, LinuxIDFileErr := os.Open(filecheck)
+	if LinuxIDFileErr != nil {
+		return "", LinuxIDFileErr
+	}
+	defer LinuxIDFile.Close()
+	ID, idReadErr := ioutil.ReadAll(LinuxIDFile)
+	if idReadErr != nil {
+		return "", idReadErr
+	}
+	return string(ID), nil
 }
